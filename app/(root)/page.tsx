6ae62ef20ede500fcebf2c0215e942271cb36e45
Ololade Sanya <ololadetimileyin3@gmail.com>
2024-11-21 21:56:05 +0100

    add ppr and cashing features into the application

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,17 +2,22 @@ import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
 import StartupCard from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
-import { STARTUPS_QUERIES } from "@/lib/queries";
+import { STARTUPS_QUERY } from "@/lib/queries";
 import { StartupTypeCard } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query;
 
   const params = { search: query || null }
 
+  const session = await auth();
 
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERIES, params })
+  console.log("session hereeeeeeeeeeee --->", session?.id)
+
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params })
 
 
   return (
